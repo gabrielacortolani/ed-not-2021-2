a68c96e5c2a0ae2ac1276a6eb47c7f9660362f1d
gabrielacortolani <cardoso.gabicardoso@gmail.com>
2021-11-18 20:33:16 -0300

    17-11 continuação da árvore binária de busca

--- lib/binary_search_tree.py
+++ lib/binary_search_tree.py
@@ -87,41 +87,159 @@ class BinarySearchTree:
 
     """
         PERCURSOS
-    """       
+    """
 
-    """       
+    """
         Método que faz o percurso em-ordem (in-order traversal)
-        1º: visita em-ordem a cubárvore esquerda
+        1º: visita em-ordem a subárvore esquerda
         2º: visita a raiz
         3º: visita em-ordem a subárvore direita
         Este percurso é utilizado quando se deseja recuperar os elementos
         da árvore em ordem
-    """       
+    """
     def in_order_traversal(self, fnCallback, root = False):
 
         if root is False: root = self.__root
 
         if root is not None:
             self.in_order_traversal(fnCallback, root.left) # 1º
-            fnCallback(root.data) # 2º
+            fnCallback(root.data)   # 2º
             self.in_order_traversal(fnCallback, root.right) # 3º
 
-    """       
-        Método que faz o percurso pré-ordem (pré-order traversal)
+    """
+        Método que faz o percurso pré-ordem (pre-order traversal)
         1º: visita a raiz
         2º: visita pré-ordem a subárvore esquerda
         3º: visita pré-ordem a subárvore direita
         Este percurso é utilizado quando se deseja copiar a árvore,
-        preservando a estrutura
-    """ 
+        preservando sua estrutura
+    """
     def pre_order_traversal(self, fnCallback, root = False):
 
         if root is False: root = self.__root
 
         if root is not None:
-            fnCallback(root.data) # 1º
+            fnCallback(root.data)   # 1º
             self.pre_order_traversal(fnCallback, root.left) # 2º
             self.pre_order_traversal(fnCallback, root.right) # 3º
+
+    """
+        Método que faz o percurso pós-ordem (post-order traversal)
+        1º: visita pós-ordem a subárvore esquerda
+        2º: visita pós-ordem a subárvore direita
+        3º: visita a raiz
+        Este percurso é utilizado para sumarizações "bottom-up" (de
+        baixo para cima, das folhas em direção à raiz) e também pode
+        ser usado no processo de remoção de um nodo da árvore
+    """
+    def post_order_traversal(self, fnCallback, root = False):
+
+        if root is False: root = self.__root
+
+        if root is not None:
+            self.post_order_traversal(fnCallback, root.left) # 1º
+            self.post_order_traversal(fnCallback, root.right) # 2º
+            fnCallback(root.data)   # 3º
+
+    """
+        Método PRIVADO que busca recursivamente por um valor na árvore
+        Retorna:
+            - o nodo que contém o valor, caso este exista
+            - None, se não for encontrado
+    """
+    def __search_node(self, root, key):
+
+        # 1º caso: árvore vazia
+        if root is None: return None
+
+        # 2º caso: o valor de key é MENOR que valor da raiz
+        # Continua a busca recursivamente pela subárvore ESQUERDA
+        if key < root.data: return self.__search_node(root.left, key)
+
+        # 3º caso: o valor de key é MAIOR que o valor da raiz
+        # Continua a busca recursivamente pela subárvore DIREITA
+        if key > root.data: return self.__search_node(root.right, key)
+
+        # 4º caso: o valor de key é IGUAL ao valor da raiz
+        # ENCONTROU O VALOR; retorna o nodo root
+        return root
+
+    """
+        Método público que retorna se um valor existe na árvore (True)
+        ou não (False)
+    """
+    def exists(self, val):
+        node = self.__search_node(self.__root, val)
+        if node is None: return False
+        else: return True
+
+    """
+        Método PRIVADO para encontrar o nodo de menor valor a partir
+        da raiz fornecida
+    """
+    def __min_node(self, root):
+        node = root
+        while node is not None and node.left is not None:
+            node = node.left
+        return node
+
+    """
+        Método PRIVADO para encontrar o nodo de maior valor a partir
+        da raiz fornecida
+    """
+    def __max_node(self, root):
+        node = root
+        while node is not None and node.right is not None:
+            node = node.right
+        return node
+
+    """
+        Método público para remoção de um valor da árvore
+    """
+    def remove(self, val):
+        self.__root = self.__remove_node(self.__root, val)
+
+    """
+        Método PRIVADO para remoção de um nodo da árvore
+    """
+    def __remove_node(self, root, val):
+
+        # 1º caso: árvore vazia
+        if root is None: return None
+
+        # 2º caso: o valor a ser removido é MENOR que o valor da raiz
+        # Continua procurando pelo nodo a ser removido pelo lado ESQUERDO
+        if val < root.data: 
+            root.left =  self.__remove_node(root. left, val)
+            return root
+
+        # 3º caso: o valor a ser removido é MAIOR que o valor da raiz
+        # Continua procurando pelo nodo a ser removido pelo lado DIREITO
+        if val > root.data:
+            root.right = self.__remove_node(root.right, val)
+            return root
+        
+        # 4º caso: o valor a ser removido é IGUAL ao valor da raiz
+        # O nodo a ser removido foi encontrado; agora é necessário
+        # determinar o grau do nodo para aplicar o algoritmo de remoção
+        # correto para cada grau
+
+        # 4.1: remoção de nodo de grau 0
+        if root.left is None and root.right is None:
+            root = None
+            return root
+
+        # 4.2: remoção de nodo de grau 1 com subárvore à esquerda
+        if root.left is not None and root.right is None:
+            root = root.left
+            return root
+
+        # 4.3: remoção do nodo de grau 1 com subárvore à direita
+        if root.left is None and root.right is not None:
+            root = root.right
+            return root
+
+        # 4.4: remoção do nodo de grau 2 
 
 
 ######################################################################
@@ -146,7 +264,7 @@ def insere_em_ordem(val):
 arvore.in_order_traversal(lambda val: em_ordem.append(val))
 #arvore.in_order_traversal(lambda val: print(val))
 
-print('Percuso em-ordem:', em_ordem)
+print('Percurso em-ordem:', em_ordem)
 
 sumario = BinarySearchTree()
 
@@ -164,4 +282,17 @@ print('Sumário em-ordem:', em_ordem)
 
 pre_ordem = []
 sumario.pre_order_traversal(lambda val: pre_ordem.append(val))
-print('Sumario pré-ordem:', pre_ordem)
+print('Sumário pré-ordem:', pre_ordem)
+
+pre_ordem = []
+arvore.pre_order_traversal(lambda val: pre_ordem.append(val))
+print('Árvore pré-ordem:', pre_ordem)
+
+pos_ordem = []
+arvore.post_order_traversal(lambda val: pos_ordem.append(val))
+print('Árvore pós-ordem:', pos_ordem)
+
+existe36 = arvore.exists(36)
+existe51 = arvore.exists(51)
+existe64 = arvore.exists(64)
+print(f'36: {existe36}, 51: {existe51}, 64: {existe64}')
